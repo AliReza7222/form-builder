204009c7c1dd1e1199c337f1f283ae6d10f8d474
AliReza7222 <ali.r.salehi99@gmail.com>
2024-12-13 01:39:29 +0330

    created api FormDetailView for getting form's details

--- backend/form_builder/quera_forms/api/urls.py
+++ backend/form_builder/quera_forms/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FormListView
+from .views import FormDetailView, FormListView
 
 urlpatterns = [
     path("forms/", FormListView.as_view(), name="forms"),
+    path("form/<int:pk>/", FormDetailView.as_view(), name="form_detail"),
 ]
--- backend/form_builder/quera_forms/api/views.py
+++ backend/form_builder/quera_forms/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
 
 from form_builder.quera_forms.models import Form
@@ -18,3 +18,8 @@ class FormListView(ListAPIView):
     )
     serializer_class = FormSerializer
     pagination_class = FormPagination
+
+
+class FormDetailView(RetrieveAPIView):
+    queryset = Form.objects.all().prefetch_related("questions")
+    serializer_class = FormSerializer
