9b22b60457c573c1f9eadb02f55625ffec51737a
AliReza7222 <ali.r.salehi99@gmail.com>
2024-12-13 21:34:44 +0330

    writing test for Response User to form

--- backend/form_builder/conftest.py
+++ backend/form_builder/conftest.py
@@ -3,7 +3,8 @@ from faker import Faker
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
-from form_builder.quera_forms.models import Form
+from form_builder.quera_forms.enums import QuestionTypeEnum
+from form_builder.quera_forms.models import Form, Question
 from form_builder.users.models import User
 
 
@@ -52,3 +53,55 @@ def create_random_form(db, fake, create_random_user):
         )
 
     return fake_form
+
+
+@pytest.fixture
+def create_questions(db, create_random_form):
+    def questions(form):
+        question_number_1 = Question.objects.create(
+            form=form,
+            question_text="Enter number?",
+            required=True,
+            type=QuestionTypeEnum.NUMBER.name,
+            max_value=100,
+            min_value=1,
+            is_decimal=False,
+        )
+        question_number_2 = Question.objects.create(
+            form=form,
+            question_text="Enter number?",
+            required=True,
+            type=QuestionTypeEnum.NUMBER.name,
+            max_value=100,
+            min_value=1,
+            is_decimal=True,
+        )
+        question_email = Question.objects.create(
+            form=form,
+            question_text="Enter email?",
+            required=True,
+            type=QuestionTypeEnum.EMAIL.name,
+        )
+        question_name = Question.objects.create(
+            form=form,
+            question_text="what's your name?",
+            required=True,
+            type=QuestionTypeEnum.SHORT_TEXT.name,
+            max_length=100,
+        )
+        question_description = Question.objects.create(
+            form=form,
+            question_text="description :",
+            required=True,
+            type=QuestionTypeEnum.LONG_TEXT.name,
+            max_length=500,
+        )
+        return [
+            question_number_1,
+            question_number_2,
+            question_email,
+            question_name,
+            question_description,
+        ]
+
+    return questions
--- backend/form_builder/quera_forms/tests/test_drf.py
+++ backend/form_builder/quera_forms/tests/test_drf.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from form_builder.quera_forms.models import Answer, Response
+
 
 @pytest.mark.django_db
 class TestFormList:
@@ -35,3 +37,136 @@ class TestFormDetail:
         response = client.get(url)
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+class TestResponseUser:
+    def test_create_response_success(
+        self, client, create_questions, create_random_form
+    ):
+        url = reverse("api:response-list")
+        form = create_random_form()
+        questions = create_questions(form)
+        data = {
+            "form": form.id,
+            "user_identifier": "testuser@example.com",
+            "answers": [
+                {"question": questions[0].id, "answer_number": 10},
+                {"question": questions[1].id, "answer_number": 10.10},
+                {
+                    "question": questions[2].id,
+                    "answer_text": "testuser@example.com",
+                },
+                {"question": questions[3].id, "answer_text": "Test"},
+                {"question": questions[4].id, "answer_text": "Hello world !"},
+            ],
+        }
+        response = client.post(url, data, format="json")
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Response.objects.count() == 1
+        assert Answer.objects.count() == 5
+
+    def test_missing_answers(
+        self, client, create_questions, create_random_form
+    ):
+        url = reverse("api:response-list")
+        form = create_random_form()
+        questions = create_questions(form)
+        data = {
+            "form": form.id,
+            "user_identifier": "testuser@example.com",
+            "answers": [
+                {"question": questions[0].id, "answer_number": 10},
+                {"question": questions[1].id, "answer_number": 10.10},
+                {
+                    "question": questions[2].id,
+                    "answer_text": "testuser@example.com",
+                },
+            ],
+        }
+
+        response = client.post(url, data, format="json")
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "required questions have not been answered" in str(
+            response.data["answers"]
+        )
+
+    def test_invalid_question_ids(
+        self, client, create_questions, create_random_form
+    ):
+        url = reverse("api:response-list")
+
+        form_1 = create_random_form()
+        form_2 = create_random_form()
+
+        questions_form_1 = create_questions(form_1)
+        questions_form_2 = create_questions(form_2)
+
+        data = {
+            "form": form_1.id,
+            "user_identifier": "testuser@example.com",
+            "answers": [
+                {"question": questions_form_1[0].id, "answer_number": 10},
+                {"question": questions_form_1[1].id, "answer_number": 10.10},
+                {
+                    "question": questions_form_1[2].id,
+                    "answer_text": "testuser@example.com",
+                },
+                {"question": questions_form_1[3].id, "answer_text": "Test"},
+                {
+                    "question": questions_form_2[4].id,
+                    "answer_text": "Hello world !",
+                },
+            ],
+        }
+
+        response = client.post(url, data, format="json")
+
+        msg_error = f"Question with ID {questions_form_2[4].id} does not belong to the specified form with ID {form_1.id}."
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert msg_error in str(response.data["answers"])
+
+    def test_invalid_answers(
+        self, client, create_questions, create_random_form
+    ):
+        url = reverse("api:response-list")
+
+        form = create_random_form()
+        questions = create_questions(form)
+        answer_short_text = "Test" * 100
+        answer_long_text = "Hello world !" * 100
+
+        data = {
+            "form": form.id,
+            "user_identifier": "testuser@example.com",
+            "answers": [
+                {"question": questions[0].id, "answer_number": 10.10},
+                {"question": questions[1].id, "answer_number": 10},
+                {"question": questions[2].id, "answer_text": "testuser.com"},
+                {"question": questions[3].id, "answer_text": answer_short_text},
+                {"question": questions[4].id, "answer_text": answer_long_text},
+            ],
+        }
+
+        response = client.post(url, data, format="json")
+        response_errors = response.data.get("answers", [])
+
+        mesg_errors = [
+            f"The answer must be an integer for the question with ID {questions[0].id}.",
+            f"The provided answer 'testuser.com' is not a valid email address for the question with ID {questions[2].id}.",
+            f"The maximum allowed length (100 characters) for the question with ID {questions[3].id}.",
+            f"The maximum allowed length (500 characters) for the question with ID {questions[4].id}.",
+        ]
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+        for error in mesg_errors:
+            found = any(
+                error in str(detail)
+                for answer in response_errors
+                for detail in answer.values()
+            )
+            assert found
