a5424ff29ad19b8ebcc9329b502008bf7dd00b5f
AliReza7222 <ali.r.salehi99@gmail.com>
2024-12-13 11:31:28 +0330

    cleaned and refactor file factory_validators

--- backend/form_builder/quera_forms/factory_validators.py
+++ backend/form_builder/quera_forms/factory_validators.py
@@ -1,26 +1,41 @@
+from abc import ABC, abstractmethod
+
 from .enums import QuestionTypeEnum
 from .validators import *
 
 
-class QuestionValidatorFactory:
-    validators = {
-        QuestionTypeEnum.SHORT_TEXT.name: ShortTextValidator(),
-        QuestionTypeEnum.LONG_TEXT.name: LongTextValidator(),
-        QuestionTypeEnum.NUMBER.name: NumberValidator(),
-    }
+class BaseValidatorFactory(ABC):
+    @abstractmethod
+    def validators(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_validator(cls, question_type):
+        pass
+
+
+class QuestionValidatorFactory(BaseValidatorFactory):
+    def validators(self):
+        return {
+            QuestionTypeEnum.SHORT_TEXT.name: ShortTextValidator(),
+            QuestionTypeEnum.LONG_TEXT.name: LongTextValidator(),
+            QuestionTypeEnum.NUMBER.name: NumberValidator(),
+        }
 
     @classmethod
     def get_validator(cls, question_type):
         return cls.validators.get(question_type, DefaultValidator())
 
 
-class AnswerValidatorFactory:
-    validators = {
-        QuestionTypeEnum.SHORT_TEXT.name: TextAnswerValidator(),
-        QuestionTypeEnum.LONG_TEXT.name: TextAnswerValidator(),
-        QuestionTypeEnum.EMAIL.name: EmailAnswerValidator(),
-        QuestionTypeEnum.NUMBER.name: NumberAnswerValidator(),
-    }
+class AnswerValidatorFactory(BaseValidatorFactory):
+    def validators(self):
+        return {
+            QuestionTypeEnum.SHORT_TEXT.name: TextAnswerValidator(),
+            QuestionTypeEnum.LONG_TEXT.name: TextAnswerValidator(),
+            QuestionTypeEnum.EMAIL.name: EmailAnswerValidator(),
+            QuestionTypeEnum.NUMBER.name: NumberAnswerValidator(),
+        }
 
     @classmethod
     def get_validator(cls, question_type):
